b27448a9b1c2da5fcb9d712601c7011415e06c62
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-06-24 18:18:42 +0900

    [add] 取得日時とバス時刻表を表示できるように変更

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app.add_middleware(
 
 @app.get("/")
 # バス時刻表を取得
-def read_bustable(from_station:str='浜松学院大学', to_station:str='浜松駅'):
+def read_bustable(from_station:str, to_station:str):
     try:
         getbustable = GetBusTable()
         result = getbustable._get_bustable(from_station, to_station)
