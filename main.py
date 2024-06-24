abb318f372e884d68dd690ff414428c265261a98
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-06-24 14:06:31 +0900

    [add] APIから任意のバス停間の到着予定と現在地のjsonファイルを取得できるHTMLファイルを作成

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from _getbustable import GetBusTable
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 # バス時刻表を取得
 def read_bustable(from_station:str='浜松学院大学', to_station:str='浜松駅'):
-    getbustable = GetBusTable()
-    result = getbustable._get_bustable(from_station, to_station)
-    return result
+    try:
+        getbustable = GetBusTable()
+        result = getbustable._get_bustable(from_station, to_station)
+        return result
+    except:
+        return None
